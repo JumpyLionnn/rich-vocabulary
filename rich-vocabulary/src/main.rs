f594933a7f5dc5212c94362e48b5a2a03549cbe4
JumpyLionnn <jumpylionn1@gmail.com>
2024-01-27 16:22:23 +0200

    Add a loop

--- rich-vocabulary/src/main.rs
+++ rich-vocabulary/src/main.rs
@@ -1,5 +1,5 @@
 use std::io::{self, Write};
-use dictionary::Dictionary;
+use dictionary::{Dictionary, Word};
 
 
 #[tokio::main]
@@ -7,41 +7,23 @@ async fn main() {
     println!("Hello, world!");
 
     let client = Dictionary::new();
-
-    let word = input("Enter a word: ").unwrap();
-
-    match client.get_definition(&word).await {
-        Ok(word) => {
-            println!("Showing definition for '{}':", word.word);
-            for meaning in word.meanings {
-                println!("    {:?}:", meaning.part_of_speech);
-                for definition in meaning.definitions {
-                    println!("        {}", definition.definition);
-                    if let Some(example) = definition.example {
-                        println!("          example: {example}");
+    loop {
+        let word = input("Enter a word: ").unwrap();
+        if word.trim().is_empty() {
+            break;
+        }
+        match client.get_definition(&word).await {
+            Ok(word) => {
+                print_definition(word);
+            },
+            Err(error) => {
+                match error {
+                    dictionary::DictionaryError::NotFound(_) => {
+                        println!("Couldn't find the word you were looking for.")
+                    },
+                    other => {
+                        println!("Encountered an error while searching for the word definition: {other:?}");
                     }
-                    if !definition.synonyms.is_empty() {
-                        println!("          synonyms: {}", definition.synonyms.join(", "));
-                    }
-                    if !definition.antonyms.is_empty() {
-                        println!("          antonyms: {}", definition.antonyms.join(", "));
-                    }
-                }
-                if !meaning.synonyms.is_empty() {
-                    println!("      synonyms: {}", meaning.synonyms.join(", "));
-                }
-                if !meaning.antonyms.is_empty() {
-                    println!("      antonyms: {}", meaning.antonyms.join(", "));
-                }
-            }
-        },
-        Err(error) => {
-            match error {
-                dictionary::DictionaryError::NotFound(_) => {
-                    println!("Couldn't find the word you were looking for.")
-                },
-                other => {
-                    println!("Encountered an error while searching for the word definition: {other:?}");
                 }
             }
         }
@@ -55,4 +37,30 @@ fn input(prompt: &str) -> io::Result<String> {
     io::stdout().flush()?;
     io::stdin().read_line(&mut line)?;
     Ok(line)
+}
+
+
+fn print_definition(word: Word) {
+    println!("Showing definition for '{}':", word.word);
+    for meaning in word.meanings {
+        println!("    {:?}:", meaning.part_of_speech);
+        for definition in meaning.definitions {
+            println!("        {}", definition.definition);
+            if let Some(example) = definition.example {
+                println!("          example: {example}");
+            }
+            if !definition.synonyms.is_empty() {
+                println!("          synonyms: {}", definition.synonyms.join(", "));
+            }
+            if !definition.antonyms.is_empty() {
+                println!("          antonyms: {}", definition.antonyms.join(", "));
+            }
+        }
+        if !meaning.synonyms.is_empty() {
+            println!("      synonyms: {}", meaning.synonyms.join(", "));
+        }
+        if !meaning.antonyms.is_empty() {
+            println!("      antonyms: {}", meaning.antonyms.join(", "));
+        }
+    }
 }
