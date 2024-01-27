2daadbce99b62c059fb745b34a2ee6f5bfda8c4f
JumpyLionnn <jumpylionn1@gmail.com>
2024-01-27 11:33:57 +0200

    Add a dictionary

---
+++ crates/dictionary/src/lib.rs
@@ -0,0 +1,203 @@
+use serde::Deserialize;
+
+const URL: &'static str = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+#[derive(Debug, Deserialize)]
+pub struct ApiWord {
+    pub word: String,
+    pub phonetic: Option<String>,
+    pub phonetics: Vec<ApiPhonetic>,
+    pub origin: Option<String>,
+    pub meanings: Vec<ApiWordMeaning>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ApiPhonetic {
+    pub text: Option<String>,
+    pub audio: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ApiWordMeaning {
+    #[serde(rename = "partOfSpeech")]
+    pub part_of_speech: String,
+    pub definitions: Vec<ApiWordDefinition>,
+    pub synonyms: Vec<String>,
+    pub antonyms: Vec<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ApiWordDefinition {
+    pub definition: String,
+    pub example: Option<String>,
+    pub synonyms: Vec<String>,
+    pub antonyms: Vec<String>,
+}
+
+#[derive(Debug)]
+pub struct Word {
+    pub word: String,
+    pub phonetic: Option<String>,
+    pub phonetics: Vec<Phonetic>,
+    pub origin: Option<String>,
+    pub meanings: Vec<WordMeaning>,
+}
+
+impl TryFrom<ApiWord> for Word {
+    type Error = UnknownPartOfSpeech;
+
+    fn try_from(word: ApiWord) -> Result<Self, Self::Error> {
+        type Partitioned = Vec<Result<WordMeaning, UnknownPartOfSpeech>>;
+        let (meanings, mut errors): (Partitioned, Partitioned) = word
+            .meanings
+            .into_iter()
+            .map(WordMeaning::try_from)
+            .partition(Result::is_ok);
+        if let Some(err) = errors.pop() {
+            Err(err.unwrap_err())
+        } else {
+            Ok(Self {
+                word: word.word,
+                phonetic: word.phonetic,
+                phonetics: word.phonetics.into_iter().map(Phonetic::from).collect(),
+                origin: word.origin,
+                meanings: meanings.into_iter().map(Result::unwrap).collect(),
+            })
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Phonetic {
+    pub text: Option<String>,
+    pub audio: Option<String>,
+}
+
+impl From<ApiPhonetic> for Phonetic {
+    fn from(phonetic: ApiPhonetic) -> Self {
+        Self {
+            text: phonetic.text,
+            audio: phonetic
+                .audio
+                .and_then(|src| if src.is_empty() { None } else { Some(src) }),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct WordMeaning {
+    pub part_of_speech: PartOfSpeech,
+    pub definitions: Vec<WordDefinition>,
+    pub synonyms: Vec<String>,
+    pub antonyms: Vec<String>,
+}
+
+impl TryFrom<ApiWordMeaning> for WordMeaning {
+    type Error = UnknownPartOfSpeech;
+
+    fn try_from(meaning: ApiWordMeaning) -> Result<Self, Self::Error> {
+        Ok(Self {
+            part_of_speech: meaning.part_of_speech.try_into()?,
+            definitions: meaning
+                .definitions
+                .into_iter()
+                .map(WordDefinition::from)
+                .collect(),
+            synonyms: meaning.synonyms,
+            antonyms: meaning.antonyms,
+        })
+    }
+}
+
+#[derive(Debug)]
+pub enum PartOfSpeech {
+    Noun,
+    Pronoun,
+    Verb,
+    Adjective,
+    Adverb,
+    Preposition,
+    Conjunction,
+    Interjection,
+}
+
+#[derive(Debug)]
+pub struct UnknownPartOfSpeech {
+    pub kind: String,
+}
+
+impl TryFrom<String> for PartOfSpeech {
+    type Error = UnknownPartOfSpeech;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match &value[..] {
+            "noun" => Ok(Self::Noun),
+            "pronoun" => Ok(Self::Pronoun),
+            "verb" => Ok(Self::Verb),
+            "adjective" => Ok(Self::Adjective),
+            "adverb" => Ok(Self::Adverb),
+            "preposition" => Ok(Self::Preposition),
+            "conjunction" => Ok(Self::Conjunction),
+            "interjection" => Ok(Self::Interjection),
+            _ => Err(UnknownPartOfSpeech { kind: value }),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct WordDefinition {
+    pub definition: String,
+    pub example: Option<String>,
+    pub synonyms: Vec<String>,
+    pub antonyms: Vec<String>,
+}
+
+impl From<ApiWordDefinition> for WordDefinition {
+    fn from(word: ApiWordDefinition) -> Self {
+        Self {
+            definition: word.definition,
+            example: word.example,
+            synonyms: word.synonyms,
+            antonyms: word.antonyms,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum DictionaryError {
+    Fetch(reqwest::Error),
+    Deserialize(reqwest::Error),
+    Conversion(UnknownPartOfSpeech),
+}
+
+pub struct Dictionary {
+    client: reqwest::Client,
+}
+
+impl Dictionary {
+    pub fn new() -> Self {
+        Self {
+            client: reqwest::Client::new(),
+        }
+    }
+
+    pub async fn get_definition(&self, word: &str) -> Result<Word, DictionaryError> {
+        let url = format!("{URL}{word}");
+        let res: reqwest::Response = self
+            .client
+            .get(url)
+            .send()
+            .await
+            .map_err(DictionaryError::Fetch)?;
+        res.json::<Vec<ApiWord>>()
+            .await
+            .map_err(DictionaryError::Deserialize)
+            .and_then(|mut words| {
+                words
+                    .pop()
+                    .unwrap()
+                    .try_into()
+                    .map_err(DictionaryError::Conversion)
+            })
+    }
+}
--- rich-vocabulary/src/main.rs
+++ rich-vocabulary/src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
+use std::io::{self, Write};
+use dictionary::Dictionary;
+
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+
+    let client = Dictionary::new();
+
+    let mut word = String::new();
+    print!("Enter a word: ");
+    io::stdout().flush().expect("Couldn't flush stdout.");
+    io::stdin().read_line(&mut word).expect("Couldn't read from stdout");
+
+    match client.get_definition(&word).await {
+        Ok(word) => {
+            println!("Showing definition for '{}':", word.word);
+            for meaning in word.meanings {
+                println!("    {:?}:", meaning.part_of_speech);
+                for definition in meaning.definitions {
+                    println!("        {}", definition.definition);
+                    if let Some(example) = definition.example {
+                        println!("          example: {example}");
+                    }
+                    if !definition.synonyms.is_empty() {
+                        println!("          synonyms: {}", definition.synonyms.join(", "));
+                    }
+                    if !definition.antonyms.is_empty() {
+                        println!("          antonyms: {}", definition.antonyms.join(", "));
+                    }
+                }
+                if !meaning.synonyms.is_empty() {
+                    println!("      synonyms: {}", meaning.synonyms.join(", "));
+                }
+                if !meaning.antonyms.is_empty() {
+                    println!("      antonyms: {}", meaning.antonyms.join(", "));
+                }
+            }
+        },
+        Err(error) => {
+            println!("Encountered an error while searching for the word definition: {error:?}");
+        }
+    }
 }
