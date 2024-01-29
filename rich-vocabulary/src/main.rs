5b90ed78c11bf99d250a24517a54bc9e92b1adaf
JumpyLionnn <jumpylionn1@gmail.com>
2024-01-29 07:58:37 +0200

    Add a basic question

--- rich-vocabulary/src/main.rs
+++ rich-vocabulary/src/main.rs
@@ -1,10 +1,12 @@
 use chrono::NaiveDateTime;
-use dictionary::{Dictionary, Word};
+use dictionary::{Dictionary, Word, WordDefinition, WordMeaning};
 use futures::stream::StreamExt;
 use sqlx::{
     migrate::MigrateDatabase, query, query_as, Either, FromRow, Pool, Row, Sqlite, SqlitePool,
 };
 use std::io::{self, Write};
+
+use rand::seq::SliceRandom;
 
 const DB_URL: &str = "sqlite://sqlite.db";
 
@@ -14,6 +16,18 @@ struct WordEntry {
     pub word: String,
     pub last_quizzed: NaiveDateTime,
     pub score: i64,
+}
+
+struct Question {
+    word_uid: i64,
+    question: String,
+    answers: Vec<Answer>,
+}
+
+#[derive(Debug)]
+struct Answer {
+    content: String,
+    correct: bool,
 }
 
 #[tokio::main]
@@ -62,17 +76,88 @@ async fn main() {
                 }
                 "practice" => {
                     let words = select_random_by_score(&db, 4).await.unwrap();
-                    for word in words {
+                    for entry in words {
                         println!(
                             "{} with {} points at {}.",
-                            word.word, word.score, word.last_quizzed
+                            entry.word, entry.score, entry.last_quizzed
                         );
+                        let word = dict.get_definition(&entry.word).await;
+                        if let Ok(word) = word {
+                            // question kind: match the definition to the correct word
+                            let meaning: &WordMeaning =
+                                word.meanings.choose(&mut rand::thread_rng()).unwrap();
+                            let definition: &WordDefinition =
+                                meaning.definitions.choose(&mut rand::thread_rng()).unwrap();
+                            let answer = definition
+                                .antonyms
+                                .choose(&mut rand::thread_rng())
+                                .or_else(|| meaning.antonyms.choose(&mut rand::thread_rng()));
+                            if let Some(answer) = answer {
+
+                                let question = Question {
+                                    word_uid: entry.uid,
+                                    question: format!(
+                                        "What word matches the following definition? {:?}",
+                                        definition.definition
+                                    ),
+                                    answers: vec![
+                                        Answer {
+                                            content: word.word,
+                                            correct: true,
+                                        },
+                                        Answer {
+                                            content: answer.clone(),
+                                            correct: false,
+                                        },
+                                    ],
+                                };
+    
+                                ask_question(&db, question).await;
+                            }
+                            // let right_answer = word.word;
+                        }
                     }
                 }
                 _ => {
                     println!("Unknown command {command}.");
                 }
             }
+        }
+    }
+}
+
+async fn ask_question(db: &Pool<Sqlite>, mut question: Question) {
+    println!("{}", question.question);
+    question.answers.shuffle(&mut rand::thread_rng());
+    for (index, answer) in question.answers.iter().enumerate() {
+        println!("[{}]: {}", index + 1, answer.content);
+    }
+    let answer = loop {
+        let chosen_answer = input("Enter the number of the correct answer: ").unwrap();
+        let chosen_answer = chosen_answer.trim();
+        if chosen_answer == "exit" {
+            break None;
+        } else if let Ok(index) = chosen_answer.parse::<usize>() {
+            if let Some(answer) = question.answers.get(index.wrapping_sub(1)) {
+                break Some(answer);
+            }
+        }
+    };
+    if let Some(answer) = answer {
+        if answer.correct {
+            println!("The answer is correct. Well done!");
+            query!("UPDATE words SET score = ROUND(score * 0.92 - 0.5) WHERE uid = ?", question.word_uid).execute(db).await.unwrap();
+        } else {
+            println!(
+                "The answer is incorrect. The right answer is {}. ",
+                question
+                .answers
+                .iter()
+                .find(|answer| answer.correct)
+                .map(|answer| &answer.content[..])
+                .unwrap_or("unknown")
+            );
+            query!("UPDATE words SET score = MIN(ROUND(score * 1.04 + 0.5), 1000) WHERE uid = ?", question.word_uid).execute(db).await.unwrap();
         }
     }
 }
